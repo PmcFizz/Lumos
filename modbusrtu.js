52475b1425aac033f3334b903c012314fc1ed4fe
Fizz Pang <pangmengchen@gmail.com>
2024-04-10 21:45:33 +0800

    feat: 调整寄存器开始位置

--- modbusrtu.js
+++ modbusrtu.js
@@ -1,3 +1,15 @@
+/**
+ *  寄存器0 - 【ID】（范围：1~255）[只能按键设置]
+ *  寄存器1 - 【波特率】（范围：9600、19200、38400）[只能按键设置]
+ *  寄存器2 - 【工作模式】（模式1、2、3）
+ *  寄存器3 - 【DIM1灰度】（范围：0~255，与模式无关）
+ *  寄存器4 - 【DIM2灰度】（范围：0~255，与模式无关）
+ *  寄存器5 - 【DIM频率】（范围：1~5kHZ，与模式无关）
+ *  寄存器6 - 【预留】
+ *  寄存器7 - 【驱动板整体灰度】（范围：1~255，模式1专用）
+ *  寄存器8 - 【寄存器有效位数】（范围：3~16，模式1专用）
+ *  寄存器9 - 【驱动板输出路数】（范围：16~111*16）
+ */
 // create an empty modbus client
 const ModbusRTU = require("modbus-serial");
 const client = new ModbusRTU();
@@ -6,15 +18,19 @@ const client = new ModbusRTU();
 client.connectRTUBuffered("/dev/tty.usbserial-110", { baudRate: 9600 }, write);
 
 function write() {
-    client.setID(1);
+  client.setID(1);
 
-    // 从第0个位置，写入4个寄存器的值
-    client.writeRegisters(0, [0xffff , 0xffff, 0x0003, 0x0001])
-        .then(read);
+  // 前9个寄存器是配置参数
+  // 从第10个位置，写入4个寄存器的值
+  client
+    // .writeRegisters(10, [0xffff, 0xffff, 0xffff, 0xffff])
+    .writeRegisters(10, [0x0000, 0x0000, 0x0000, 0x0000])
+    .then(read);
 }
 
 function read() {
-    // 从第0个位置，读取5个寄存器的值
-    client.readHoldingRegisters(0, 5)
-        .then(console.log);
+  // 从第0个位置，读取5个寄存器的值
+  client
+    .readHoldingRegisters(10, 5)
+    .then(console.log);
 }
