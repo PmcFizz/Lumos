9caf6ae41bea41647cb5b376d701aa0c8b7968f8
Fizz Pang <pangmengchen@gmail.com>
2024-04-22 22:50:00 +0800

    feat: 文件迁移

--- obfuscate.js
+++ obfuscate.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const { minify } = require("terser");
+// const htmlMinifier = require("html-minifier").minify;
 
-const options = {
+const optionsJS = {
   compress: {
     drop_console: true, // 移除console语句
     drop_debugger: true, // 移除debugger语句
@@ -15,19 +16,44 @@ const options = {
     beautify: false, // 不美化输出
   },
 };
-async function obfuscateDir(dir) {
+
+const optionsHTML = {
+  removeComments: true,
+  collapseWhitespace: true,
+  minifyJS: true,
+  minifyCSS: true,
+};
+
+async function obfuscateDir(dir, outputDir) {
   const files = fs.readdirSync(dir);
   for (const file of files) {
     const filePath = path.join(dir, file);
+    const outputPath = path.join(outputDir, path.relative("./src", filePath));
+    const outputDirPath = path.dirname(outputPath);
+
+    // Ensure output directory exists
+    if (!fs.existsSync(outputDirPath)) {
+      fs.mkdirSync(outputDirPath, { recursive: true });
+    }
+
     if (fs.statSync(filePath).isDirectory()) {
-      await obfuscateDir(filePath); // 递归目录
+      await obfuscateDir(filePath, outputDir); // 递归目录
     } else if (path.extname(file) === ".js") {
       console.log(`Obfuscating ${filePath}...`);
       const originalCode = fs.readFileSync(filePath, "utf-8");
-      const result = await minify(originalCode, options);
-      fs.writeFileSync(path.join(dir, "../dist", filePath), result.code); // 写入混淆后的代码
+      const result = await minify(originalCode, optionsJS);
+      fs.writeFileSync(outputPath, result.code); // 写入混淆后的代码
+    } else if (path.extname(file) === ".html") {
+      console.log(`Minifying ${filePath}...`);
+      const originalCode = fs.readFileSync(filePath, "utf-8");
+      // const result = htmlMinifier(originalCode, optionsHTML);
+      fs.writeFileSync(outputPath, originalCode); // 写入压缩后的 HTML
     }
   }
 }
 
-obfuscateDir("./src"); // 替换 './src' 为您的源码目录
+// Example directories to be obfuscated/minified
+const directories = ["./src", "./public"]; // Add more directories as needed
+directories.forEach((dir) => {
+  obfuscateDir(dir, "./dist");
+});
